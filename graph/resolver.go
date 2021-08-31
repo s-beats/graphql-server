4fa1d2aed7fd8a70dedc7c40861976882f13ac6b
s-beats <sakashin10291029@gmail.com>
2021-08-31 23:01:30 +0900

    feat: resover redis

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,11 +1,16 @@
 package graph
 
-import "database/sql"
+import (
+	"database/sql"
+
+	redis "github.com/go-redis/redis/v8"
+)
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
-	DB *sql.DB
+	DB    *sql.DB
+	Redis *redis.Client
 }
